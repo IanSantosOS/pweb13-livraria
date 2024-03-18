1bab0da04ccaba3c30bb7d7b61b19f8d62ec5073
Ian Santos <64675360+IanSantosOS@users.noreply.github.com>
2024-03-18 16:36:03 -0300

    Update logoutRoute.js

--- routes/logoutRoute.js
+++ routes/logoutRoute.js
@@ -8,7 +8,7 @@ router.post('/', (req, res) => {
     if (err) {
       res.status(500).send('Erro ao sair da conta!');
     } else {
-      req.clearCookie('username');
+      res.clearCookie('username');
       res.redirect('/login');
     }
   });
